e601ea9de48ca3846216be39042cde6bf7677604
AlexDeGhetto <123202084+AlexDeGhetto@users.noreply.github.com>
2023-01-20 14:19:06 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Orange County Community College" << endl;
+	cout << "lab1" << endl;
+	int x;
+	int y;
+	x = 20;
+	y = 5;
+	cout << "X + Y = " << x + y << endl;
+
+	return 0;
+}
